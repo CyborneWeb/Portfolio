faf76b20b9d50201561eca1d5cbb392808e72fa0
Matic Štucin <matic.stucin@gmail.com>
2025-07-30 17:19:15 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div className="font-semibold text-purple-600 bg-purple-100 rounded-lg px-2 py-1 shadow-lg">Hi there</div>
-    </>
-  )
+  return <> <Navbar />
+  </>;
 }
 
-export default App
+export default App;
